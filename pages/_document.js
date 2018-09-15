3fffb6ef4911cd35737a35fb8bf8122474c80a72
Marcos Renato <marcosrenatovnunes@hotmail.com>
2018-09-15 00:05:51 -0300

    fonts+assets

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.8.2/antd.min.css" />
                 <link rel="stylesheet" href="/_next/static/style.css" />
                 <link rel="stylesheet" href="./static/css/font-use.css" />
-                <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700" rel="stylesheet"></link>
+                < link href = "https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel = "stylesheet" >
             </head>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
---
+++ pages/login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Row, Col} from 'antd';
+
+class LoginPage extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>testando aaa</h1>
+                <h2>testando bbb</h2>
+                <style jsx> {`
+                    h2{
+                        font-family: 'friz';
+                    }
+                `}</style>
+            </div>
+        )
+    }
+}
+
+export default LoginPage;

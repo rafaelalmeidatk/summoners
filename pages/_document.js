79d953e1c3cd6066081129b9a2b0473fdf058da1
Rafael Almeida <rafaelalmeidatk@gmail.com>
2018-09-15 00:47:48 -0300

    Fix css imports webpack config

--- pages/_document.js
+++ pages/_document.js
@@ -3,22 +3,24 @@ import Document, { Head, Main, NextScript } from 'next/document';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class MyDocument extends Document {
-    render() {
-        return <html>
-                    <head>
-                        <link rel="stylesheet" href="/_next/static/style.css" />
-                        <link rel="stylesheet" href="./static/css/font-use.css" />
-                        <link href = "https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel = "stylesheet" />
-                    </head>
-                    <Head>
-                        <meta name="viewport" content="width=device-width, initial-scale=1" />
-                        <meta charSet="utf-8" />
-                    </Head>
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <link rel="stylesheet" href="/_next/static/style.css" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700"
+            rel="stylesheet"
+          />
+        </Head>
 
-                    <body>
-                        <Main />
-                        <NextScript />
-                    </body>
-                </html>
-    }
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
 }

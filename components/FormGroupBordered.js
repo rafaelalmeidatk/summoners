72192545aa2d7a9b63750f4b247b7b057dc208e4
Rafael Almeida <rafaelalmeidatk@gmail.com>
2018-09-15 14:49:39 -0300

    Fix FormGroupBordered

--- components/FormGroupBordered.js
+++ components/FormGroupBordered.js
@@ -1,53 +1,50 @@
-import React from "react";
-import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import React from 'react';
+import { FormGroup, Label, Input } from 'reactstrap';
 
 class FormGroupBordered extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      borderActive: false
-    };
-  }
-
-  changeBorder = () => this.setState({ borderActive: this.state.borderActive ? false : true });
-
   render() {
-    return <FormGroup className="formGroup">
-        <Label>{this.props.label}</Label>
-        <Input type={this.props.type} placeholder={this.props.placeholder} />
-        <style jsx global>
-          {`
-            .formGroup {
-              border-left: ${this.state.borderActive ? "2px solid red" : "0"};
-              margin-bottom: 45px;
-            }
-            .formGroup label {
-              color: #33434e;
-              margin-bottom: 0;
-            }
-            .formGroup input {
-              background: transparent;
-              border: none;
-              border-radius: 0;
-              border-bottom: 1px solid #33434e;
-              padding: 0;
-              font-size: 20px;
-              color: #fff;
-            }
-            .form-control:focus {
-              color: #fff;
-              background-color: transparent;
-              border: none;
-              outline: 0;
-              box-shadow: none;
-            }
-            .formGroup input: hover, .formGroup input:active, .formGroup input:focus, .form-control:focus {
-              background: transparent !important;
-              box-shadow: none !important;
-              border: none !important;
-            }`}
-        </style>
-      </FormGroup>;
+    const { label, ...props } = this.props;
+
+    return (
+      <FormGroup className="form-group">
+        <Label>{label}</Label>
+        <Input {...props} className="input" />
+
+        <style jsx global>{`
+          .form-control, .form-control:focus {
+            color: #fff !important;
+            background-color: transparent;
+            border-width: none;
+            outline: 0 !important;
+            box-shadow: none !important;
+          }
+
+          .input {
+            background: transparent;
+            border: none;
+            border-radius: 0;
+            border-bottom: 1px solid #33434e;
+            padding: 0;
+            color: #fff;
+            transition: border-width 0.1s;
+          }
+
+          .input:focus {
+            border-color: #526979;
+            border-bottom-width: 2px;
+          }
+
+          .form-group {
+            margin-bottom: 45px;
+          }
+
+          .form-group label {
+            color: #33434e;
+            margin-bottom: 0;
+          }
+        `}</style>
+      </FormGroup>
+    );
   }
 }
 

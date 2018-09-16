dbd782068fd2ee155652909a333fd3d92ec8e37b
Rafael Almeida <rafaelalmeidatk@gmail.com>
2018-09-16 19:37:25 -0300

    Add link on Logo component

--- components/Logo.js
+++ components/Logo.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default () => (
   <div>
-    Summoners
+    <Link href="/" prefetch>
+      <a className="link">Summoners</a>
+    </Link>
     <style jsx>{`
       div {
         margin-bottom: 48px;
         font-family: 'Montserrat', 'Roboto', 'Helvetica', 'Arial', sans-serif;
         font-size: 2.7em;
         font-weight: bold;
+      }
+      .link {
+        color: #fff;
+        text-decoration: none;
       }
     `}</style>
   </div>
